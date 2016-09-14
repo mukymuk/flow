c35203f2f58181cb9107b585c59248cc53a13314
Shawn Brooks <mukymuk@gmail.com>
2016-09-14 11:26:00 -0500

    added docs and debug working

--- se/debug.cmd
+++ se/debug.cmd
@@ -1,3 +1,3 @@
 #!/bin/sh
 set PATH=%PATH%;C:\Maxim\Toolchain\msys\1.0\bin
-start C:\Maxim\Toolchain\bin\openocd.exe -s C:\Maxim\Toolchain\share\openocd\scripts -f interface/ftdi/olimex-arm-usb-tiny-h.cfg -f target/MAX3263x.cfg -c init -c "reset init" -c "flash erase_sector 0 0 255"
+start C:\Maxim\Toolchain\bin\openocd.exe -s C:\Maxim\Toolchain\share\openocd\scripts -f interface/ftdi/olimex-arm-usb-tiny-h.cfg -f target/MAX3263x.cfg -c init -c "reset init" -c "flash erase_sector 0 0 255" -c "adapter_khz 4000"
